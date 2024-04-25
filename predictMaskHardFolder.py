178f34e3ec048227eb9086ba73d32713d31235a7
Wraith183 <omar.ali112511@gmail.com>
2024-04-24 19:57:09 -0700

    new file:   MaskIdentificationModel.keras 	new file:   plot.png 	modified:   predictMaskHardFolder.py

--- predictMaskHardFolder.py
+++ predictMaskHardFolder.py
@@ -8,9 +8,9 @@ larger_input = image_size + (3,)
 batch_size = 128
 num_classes = 2
 
-if os.path.exists("model5Binary.keras"):
+if os.path.exists("MaskIdentificationModel.keras"):
     # Load the weights if the file exists
-    model = load_model("model5Binary.keras")
+    model = load_model("MaskIdentificationModel.keras")
     print("Previous weights found, using those")
 else:
     print("no model found")
