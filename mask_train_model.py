eaeb01610d5651178190bfa7d2975eee250202c6
Wraith183 <omar.ali112511@gmail.com>
2024-04-24 17:23:31 -0700

    modified:   mask_train_model.py

--- mask_train_model.py
+++ mask_train_model.py
@@ -39,7 +39,6 @@ val_ds = datagen.flow_from_directory(
 # Model creation
 def create_model(input_shape, num_classes):
     inputs = keras.Input(shape=input_shape)
-
     x = layers.Rescaling(1.0 / 255)(inputs)
     x = layers.Conv2D(128, 3, strides=2, padding="same")(x)
     x = layers.BatchNormalization()(x)
@@ -68,20 +67,23 @@ def create_model(input_shape, num_classes):
 
     x = layers.GlobalAveragePooling2D()(x)
     if num_classes == 2:
+        output_activation = 'sigmoid'
         units = 1
     else:
+        output_activation = 'softmax'
         units = num_classes
 
     x = layers.Dropout(0.25)(x)
-    outputs = layers.Dense(units, activation="sigmoid")(x)  
+    outputs = layers.Dense(units, activation=output_activation)(x)
     model = keras.Model(inputs, outputs)
-    model.compile(loss='binary_crossentropy', metrics=['accuracy'])  
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
 # Model loading or creation
-modelName = "model5Binary"
-if os.path.exists(f"{modelName}.keras"):
-    model = load_model(f"{modelName}.keras")
+modelName = "MaskIdentificationModel"
+model_file_path = f"{modelName}.keras"
+if os.path.exists(model_file_path):
+    model = load_model(model_file_path)
     print("Previous weights found, using those")
 else:
     model = create_model(input_shape=image_size + (3,), num_classes=2)
@@ -91,16 +93,34 @@ else:
 model.summary()
 
 # Train the model
-epochs = 1
-loops = 15
+epochs = 3
+history = model.fit(train_ds, epochs=epochs, validation_data=val_ds, shuffle=True)
 
-for i in range(loops):
-    model.fit(
-    train_ds,
-    epochs=epochs,
-    validation_data=val_ds,
-    shuffle=True,
-    )
+# Save the trained model
+model.save(model_file_path)
 
-    # Save the trained model
-    model.save(f"{modelName}.keras")
+# Plot training and validation accuracy and losses
+fig, ax = plt.subplots(1, 2, figsize=(12, 4))
+
+ax[0].plot(history.history['accuracy'], label='Train')
+ax[0].plot(history.history['val_accuracy'], label='Validation')
+ax[0].set_title('Training Accuracy vs Validation Accuracy')
+ax[0].set_ylabel('Accuracy')
+ax[0].set_xlabel('Epoch')
+ax[0].legend(loc='upper left')
+
+ax[1].plot(history.history['loss'], label='Train')
+ax[1].plot(history.history['val_loss'], label='Validation')
+ax[1].set_title('Training Loss vs Validation Loss')
+ax[1].set_ylabel('Loss')
+ax[1].set_xlabel('Epoch')
+ax[1].legend(loc='upper left')
+
+plt.savefig("plot.png")
+
+plt.show()
+
+# Evaluate the model on training and validation sets
+train_loss, train_acc = model.evaluate(train_ds)
+val_loss, val_acc = model.evaluate(val_ds)
+print(f"Final train accuracy = {train_acc * 100:.2f}%, Validation accuracy = {val_acc * 100:.2f}%")
